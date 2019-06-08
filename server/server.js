17516fc9cea168b1d152851a19dca231a16d2ea4
Crispy <nshimyumukizachristian@gmail.com>
2019-06-08 11:56:57 +0200

    feature(api):add new PI endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import signupRouter from './routes/signup';
 import signin from './routes/signin';
+import newCar from './router/newCar';
 
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 app.use('/api/v1/auth/signup', signupRouter);
 
 app.use('/api/v1/auth/signin',signin);
+
+app.use('/api/v1/car',newCar);
 
 const port = process.env.PORT || 5101;
 
